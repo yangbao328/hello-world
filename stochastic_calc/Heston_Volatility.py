a7f7435c65a17bff9ffa512e30bd4383e6d28456
Yang Bao <93554411+yangbao328@users.noreply.github.com>
2026-07-13 21:26:23 -0500

    heston SDE verification

--- stochastic_calc/Heston_Volatility.py
+++ stochastic_calc/Heston_Volatility.py
@@ -9,9 +9,13 @@ Heston Model, stochastic volatility
 a. St and vt, 13 July
 b. Monte-Carlo multi-path European call pricer, 14 July
 c. Implied volatility and plots
+"""
 
+""""
+UNDER Q-probability measurement
 dSt = mu*St*dt + vt**0.5*St*dWt^S
 dvt = k(theta-vt)dt + sigmav*vt**0.5*dWt^v
+
 Parameters
  kappa: rate at which volatility mean-reverts to its long-term average
  theta: long-run average variance of asset
@@ -42,23 +46,31 @@ dt = T/N
 
 St = np.zeros(N+1); Vt = np.zeros(N+1)
 St[0] = S0; Vt[0] = v0
-for i in range(N):
-    Z1 = rng.normal(mu_dWtS, sigma_dWts, )
-    Z2 = rng.normal(mu_dWtS, sigma_dWts, )
-    dWtS = (dt)**0.5*Z1
-    dWtv = (dt)**0.5*(rho*Z1+(1-rho**2)**0.5*Z2)
-    Vt[i+1] = max(Vt[i] + kappa * (theta - Vt[i]) * dt + sigmav * Vt[i]**0.5 * dWtv,0)
-    St[i+1] = St[i] + r * St[i] * dt + Vt[i]**0.5 * St[i] * dWtS
+n_p = 10000
+ST = np.zeros(n_p)
+for each in range(n_p):             #verify Heston SDE simulation
     
-print(St[-1])
+    St = np.zeros(N+1); 
+    Vt = np.zeros(N+1)
+    St[0] = S0; Vt[0] = v0
+    
+    for i in range(N):
+        Z1 = rng.normal(0, 1)       #N~(0,1)
+        Z2 = rng.normal(0, 1)
+        dWtS = (dt)**0.5*Z1         #N~(0,deltat**0.5)
+        dWtv = (dt)**0.5*(rho*Z1+(1-rho**2)**0.5*Z2)
+        Vt[i+1] = np.maximum(Vt[i] + kappa * (theta - Vt[i]) * dt + sigmav * Vt[i]**0.5 * dWtv,0)
+        St[i+1] = St[i] + r * St[i] * dt + Vt[i]**0.5 * St[i] * dWtS
+    ST[each] = St[-1]
+print("Mean S_T:", np.mean(ST)) 
 
 
-
+#%%
 #vt+i = vti + kappa(theta - vti)*deltat + sigmav (vti)**0.5 *dWtv; max(vt,0)
 #St+i = Sti + rSti*deltat + vti**0.5 Sti dWtS
 
-Z1 = np.random.normal(mu_dWtS, sigma_dWts, N)
-Z2 = np.random.normal(mu_dWtS, sigma_dWts, N)
+Z1 = np.random.normal(mu_dWtS, 1, N)
+Z2 = np.random.normal(mu_dWtS, 1, N)
 
 dWtS = (dt)**0.5*Z1
 dWtv = (dt)**0.5*(rho*Z1+(1-rho**2)**0.5*Z2)
